6f92020ff7ba22099171612d0c94186c9c80d293
Denis Nikolaev <denis_nikolayev@epam.com>
2016-02-24 20:50:28 +0600

    First

---
+++ Scripts/app.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from "react"
+import * as ReactDom from "react-dom";
+
+export class App extends React.Component<any,any> {
+    render():any {
+        return <h1>hello</h1>;
+    }
+}
+
+ReactDom.render(React.createElement(App), document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: '',
+    entry: [
+         'webpack-dev-server/client?http://localhost:3000',
+         'webpack/hot/only-dev-server',
+        './scripts/app.tsx'
+    ],
+    output: {
+        path: path.join(__dirname, 'wwwroot'),
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.tsx', '.js', '']
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loaders: ['react-hot', 'ts-loader'],
+            include: path.join(__dirname, 'scripts')
+        }]
+    },
+    debug: true
+};
